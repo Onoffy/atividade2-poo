e0b6d96ca2a70f6161ea28e552b63285b6a1820a
Onoffy <thierryalmeida834@gmail.com>
2025-08-20 16:35:04 -0300

    atividade 2 poo

---
+++ src/Questao06.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Questao06 {
+    public static void main(String[] args) {
+
+        Scanner usuario = new Scanner(System.in);
+
+        System.out.print("Digite sua idade: ");
+        int idade = usuario.nextInt();
+
+        if (idade >= 0 && idade <= 12) {
+            System.out.println("Criança");
+        }else if (idade >= 13 && idade <= 17) {
+            System.out.println("Adolescente");
+        }else if (idade >= 18 && idade <= 59) {
+            System.out.println("Adulto");
+        }else{
+            System.out.println("Idoso");
+        }
+
+    }
+}
